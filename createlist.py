e8d5dd022e0c4840e232294858fcc878b3c2f40d
DOLAI BHARATH <bharathdolai5@gmail.com>
2026-04-23 14:59:20 +0530

    Delete createlist.py

--- createlist.py
+++
@@ -1,6 +0,0 @@
-a=[1,2,3,4]                     #create list
-b=["apple","banana","mango"]    #access list
-print("list:",a)
-print("first fruit:",b[0],b[1])
-print("second fruit:",b[1])
-print("third fruit:",b[2])
